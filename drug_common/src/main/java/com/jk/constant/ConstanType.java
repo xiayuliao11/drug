4e19df120c4d193d5e02736ede2207b12c3879f3
chaohy <3177900695@qq.com>
2019-03-08 21:16:37 +0800

    chaohy 上传实体Bean及常量类

---
+++ drug_common/src/main/java/com/jk/constant/ConstanType.java
@@ -0,0 +1,14 @@
+package com.jk.constant;
+
+public class ConstanType {
+
+    /**
+     * 原密码正确
+     */
+    public static final Boolean PASSWORD_STATUS_RIGHT  = true;
+
+    /**
+     * 密码错误
+     */
+    public static final Boolean PASSWORD_STATUS_WRONG = false;
+}
