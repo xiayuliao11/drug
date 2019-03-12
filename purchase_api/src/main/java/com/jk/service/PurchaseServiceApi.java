c479824b128df75c8bf4017fdc7f5c29b37161dc
dzq <2350329712@qq.com>
2019-03-12 09:57:41 +0800

    dzq 白热化阶段

--- purchase_api/src/main/java/com/jk/service/PurchaseServiceApi.java
+++ purchase_api/src/main/java/com/jk/service/PurchaseServiceApi.java
@@ -1,13 +1,30 @@
 package com.jk.service;
 
+import com.jk.pojo.PurchaseOrder;
+import com.jk.pojo.ShoppingCart;
 import com.jk.pojo.SupplierDrugs;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface PurchaseServiceApi {
 
     @GetMapping("querySupplierDrugs")
-    List<SupplierDrugs> querySupplierDrugs(SupplierDrugs supplierDrugs);
+    HashMap<String, Object> querySupplierDrugs(@RequestParam("page") Integer page,@RequestParam("rows") Integer rows, SupplierDrugs supplierDrugs);
+
+    @GetMapping("querySuppliers")
+    List<SupplierDrugs> querySuppliers();
+
+    @GetMapping("queryShoppingCart")
+    HashMap<String, Object> queryShoppingCart(@RequestParam("page") Integer page,@RequestParam("rows") Integer rows, ShoppingCart shoppingCart);
+
+    @GetMapping("queryPurchaseOrder")
+    List<PurchaseOrder> queryPurchaseOrder(PurchaseOrder purchaseOrder);
+
+    @GetMapping("queryToCart")
+    SupplierDrugs queryToCart(@RequestParam("id")Integer id);
+
+    @PostMapping("addToCart")
+    void addToCart(@RequestBody ShoppingCart shoppingCart);
 }
--- purchase_server/src/main/java/com/jk/controller/PurchaseControllerServer.java
+++ purchase_server/src/main/java/com/jk/controller/PurchaseControllerServer.java
@@ -1,13 +1,14 @@
 package com.jk.controller;
 
 import com.jk.mapper.PurchaseMapper;
+import com.jk.pojo.PurchaseOrder;
+import com.jk.pojo.ShoppingCart;
 import com.jk.pojo.SupplierDrugs;
 import com.jk.service.PurchaseServiceApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,52 @@ public class PurchaseControllerServer implements PurchaseServiceApi {
 
     @Override
     @RequestMapping("querySupplierDrugs")
-    public List<SupplierDrugs> querySupplierDrugs(@RequestBody SupplierDrugs supplierDrugs) {
-        return purchaseMapper.querySupplierDrugs(supplierDrugs);
+    public HashMap<String, Object> querySupplierDrugs(Integer page,Integer rows, @RequestBody SupplierDrugs supplierDrugs) {
+        HashMap<String, Object> map = new HashMap<>();
+        int total = purchaseMapper.querySupplierDrugsCount(supplierDrugs); //总条数
+        int start = (page-1)*rows;             //开始条数
+        List<SupplierDrugs> list = purchaseMapper.querySupplierDrugsPage(start,rows,supplierDrugs);
+        map.put("total", total);
+        map.put("rows", list);
+        return map;
     }
+
+    @Override
+    @RequestMapping("querySuppliers")
+    public List<SupplierDrugs> querySuppliers() {
+        return purchaseMapper.querySuppliers();
+    }
+
+    @Override
+    @RequestMapping("queryShoppingCart")
+    public HashMap<String, Object> queryShoppingCart(Integer page, Integer rows,@RequestBody ShoppingCart shoppingCart) {
+        HashMap<String, Object> map = new HashMap<>();
+        int total = purchaseMapper.queryShoppingCartCount(shoppingCart); //总条数
+        int start = (page-1)*rows;             //开始条数
+        List<SupplierDrugs> list = purchaseMapper.queryShoppingCartPage(start,rows,shoppingCart);
+        map.put("total", total);
+        map.put("rows", list);
+        return map;
+    }
+
+    @Override
+    @RequestMapping("queryPurchaseOrder")
+    public List<PurchaseOrder> queryPurchaseOrder(@RequestBody PurchaseOrder purchaseOrder) {
+        return purchaseMapper.queryPurchaseOrder(purchaseOrder);
+    }
+
+
+    @Override
+    @RequestMapping("queryToCart")
+    public SupplierDrugs queryToCart(Integer id) {
+        return purchaseMapper.queryToCart(id);
+    }
+
+    @Override
+    @RequestMapping("addToCart")
+    public void addToCart(@RequestBody ShoppingCart shoppingCart) {
+        purchaseMapper.addToCart(shoppingCart);
+    }
+
+
 }
