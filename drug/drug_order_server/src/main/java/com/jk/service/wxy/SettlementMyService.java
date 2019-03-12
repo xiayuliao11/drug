ce7d6c0be55e4c51b43d43948d202a993e74054e
nameVal <1460301590@qq.com>
2019-03-12 21:12:32 +0800

    王新元提交的项目模块1

--- drug/drug_order_server/src/main/java/com/jk/service/wxy/SettlementMyService.java
+++ drug/drug_order_server/src/main/java/com/jk/service/wxy/SettlementMyService.java
@@ -1,14 +1,32 @@
 package com.jk.service.wxy;
 
+import com.jk.pojo.CardBean;
+import com.jk.pojo.CountBean;
+import com.jk.pojo.RecordBean;
 import com.jk.pojo.SettlementBean;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface SettlementMyService {
 
-    List<SettlementBean> settlementList();
+    List<SettlementBean> settlementList( @RequestBody SettlementBean settlementBean);
 
     int settlementPaymentList();
 
     int settlementCashList();
+
+
+    void settlementById(Integer id);
+
+
+    List<RecordBean> querySettlementHistoryList();
+
+    int amountWithdrawn();
+
+    void saveBankCard(@RequestBody CardBean cardBean);
+
+    List<SettlementBean> setTest();
+
+    List<CountBean> stat();
 }
--- drug/drug_order_server/src/main/java/com/jk/service/wxy/SettlementServiceImpl.java
+++ drug/drug_order_server/src/main/java/com/jk/service/wxy/SettlementServiceImpl.java
@@ -1,10 +1,16 @@
 package com.jk.service.wxy;
 
 import com.jk.mapper.wxy.SettlementMapper;
+import com.jk.pojo.CardBean;
+import com.jk.pojo.CountBean;
+import com.jk.pojo.RecordBean;
 import com.jk.pojo.SettlementBean;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.List;
 @Service
 public class SettlementServiceImpl implements SettlementMyService {
@@ -12,8 +18,8 @@ public class SettlementServiceImpl implements SettlementMyService {
     private SettlementMapper settlementMapper;
 
     @Override
-    public List<SettlementBean> settlementList() {
-        return  settlementMapper.settlementList();
+    public List<SettlementBean> settlementList(SettlementBean settlementBean) {
+        return  settlementMapper.settlementList(settlementBean);
     }
 
     @Override
@@ -24,6 +30,49 @@ public class SettlementServiceImpl implements SettlementMyService {
     @Override
     public int settlementCashList() {
         return settlementMapper.settlementCashList();
+    }
+
+
+
+    @Override
+    public void settlementById(@RequestParam("id") Integer id) {
+        SettlementBean  checkInfo = settlementMapper.querySettlementInfoById(id);
+        RecordBean recordBean =new RecordBean();
+        recordBean.setRecordCommodity(checkInfo.getSettlementNumber());
+        recordBean.setRecordAmount(checkInfo.getSettlementPayment());
+        recordBean.setRecordStatus(checkInfo.getSettlementState());
+        recordBean.setRecordTime(new Date());
+        Date now = new Date();
+        Date sub = DateUtils.addHours(now, 1);//给当前时间加一小时
+        recordBean.setRecordSettling(sub);
+        settlementMapper.saveSettlementHistory(recordBean);
+
+        settlementMapper.settlementById(id);
+    }
+
+    @Override
+    public List<RecordBean> querySettlementHistoryList() {
+        return settlementMapper.querySettlementHistoryList();
+    }
+
+    @Override
+    public int amountWithdrawn() {
+        return settlementMapper.amountWithdrawn();
+    }
+
+    @Override
+    public void saveBankCard(CardBean cardBean) {
+            settlementMapper.saveBankCard(cardBean);
+    }
+
+    @Override
+    public List<SettlementBean> setTest() {
+        return settlementMapper.setTest();
+    }
+
+    @Override
+    public List<CountBean> stat() {
+        return settlementMapper.stat();
     }
 
 
