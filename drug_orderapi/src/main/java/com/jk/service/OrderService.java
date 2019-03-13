ab6bd49c6553aa696cfa41c6c4ad2820c3601c44
1969321229 <1969321229@qq.com>
2019-03-13 11:58:02 +0800

    12

--- drug_orderapi/src/main/java/com/jk/service/OrderService.java
+++ drug_orderapi/src/main/java/com/jk/service/OrderService.java
@@ -1,9 +1,10 @@
 package com.jk.service;
 
-import com.jk.pojo.GoodsReturned;
-import com.jk.pojo.SendPage;
+import com.jk.pojo.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -20,4 +21,13 @@ public interface OrderService {
 
     @RequestMapping("getDemand")
     SendPage getDemand(Map<String, Object> map);
+
+    @GetMapping("getDaugType")
+    List<DaugType> getDaugType(@RequestParam("pid") Integer pid);
+
+    @GetMapping("getRegionType")
+    List<RegionType> getRegionType(@RequestParam("id") Integer id);
+
+    @PostMapping("addDemand")
+    void addDemand(Demand t);
 }
