6680ead0f2778a64852b2710f775539cb300e50f
dzq <2350329712@qq.com>
2019-03-13 09:38:08 +0800

    dzq 收尾

--- purchase_server/src/main/java/com/jk/mapper/PurchaseMapper.java
+++ purchase_server/src/main/java/com/jk/mapper/PurchaseMapper.java
@@ -3,27 +3,45 @@ package com.jk.mapper;
 import com.jk.pojo.PurchaseOrder;
 import com.jk.pojo.ShoppingCart;
 import com.jk.pojo.SupplierDrugs;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 public interface PurchaseMapper {
 
-    int querySupplierDrugsCount(@Param("supplierDrugs")SupplierDrugs supplierDrugs);
+    int querySupplierDrugsCount(@Param("supplierDrugs") SupplierDrugs supplierDrugs);
 
-    List<SupplierDrugs> querySupplierDrugsPage(@Param("start")int start,@Param("rows")Integer rows,@Param("supplierDrugs")SupplierDrugs supplierDrugs);
+    List<SupplierDrugs> querySupplierDrugsPage(@Param("start") int start, @Param("rows") Integer rows, @Param("supplierDrugs") SupplierDrugs supplierDrugs);
 
-    @Select("select id,manufacturer from t_supplier_grugs")
+    @Select("select id,manufacturer from t_supplier_grugs group by manufacturer")
     List<SupplierDrugs> querySuppliers();
 
-    int queryShoppingCartCount(@Param("shoppingCart")ShoppingCart shoppingCart);
+    @Select("select id,subtotal from shopping_cart")
+    List<ShoppingCart> queryConsumption();
 
-    List<SupplierDrugs> queryShoppingCartPage(@Param("start")int start,@Param("rows")Integer rows,@Param("shoppingCart")ShoppingCart shoppingCart);
+    int queryShoppingCartCount(ShoppingCart shoppingCart);
 
-    List<PurchaseOrder> queryPurchaseOrder(@Param("purchaseOrder")PurchaseOrder purchaseOrder);
+    List<SupplierDrugs> queryShoppingCartPage(@Param("start") int start, @Param("rows") Integer rows, @Param("shoppingCart") ShoppingCart shoppingCart);
 
-    SupplierDrugs queryToCart(@Param("id")Integer id);
+    List<PurchaseOrder> queryPurchaseOrder(@Param("purchaseOrder") PurchaseOrder purchaseOrder);
+
+    SupplierDrugs queryToCart(@Param("id") Integer id);
 
     void addToCart(ShoppingCart shoppingCart);
+
+    @Update("update t_supplier_grugs set stock=stock-#{boxCount} where id=#{id}")
+    void updateSupplierDrugs(@Param("id") Integer id, @Param("boxCount") Integer boxCount);
+
+    void addPurchaseOrder(PurchaseOrder purchaseOrder);
+
+    @Select("select id,manufacturer from purchase_order group by manufacturer")
+    List<PurchaseOrder> queryOrderManufacturer();
+
+    @Delete("delete from shopping_cart where id=#{id}")
+    void deleteOrder(@Param("id") Integer id);
 }
--- purchase_web/src/main/java/com/jk/controller/PurchaseController.java
+++ purchase_web/src/main/java/com/jk/controller/PurchaseController.java
@@ -6,10 +6,14 @@ import com.jk.pojo.SupplierDrugs;
 import com.jk.service.PurchaseServiceWeb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 //@RequestMapping("select")
@@ -50,9 +54,9 @@ public class PurchaseController {
      * @param supplierDrugs
      * @return
      */
-    @GetMapping("querySupplierDrugs")
+    @RequestMapping("querySupplierDrugs")
     @ResponseBody
-    public HashMap<String, Object> querySupplierDrugs(@RequestParam("page") Integer page, @RequestParam("rows")Integer rows, @RequestBody SupplierDrugs supplierDrugs){
+    public HashMap<String, Object> querySupplierDrugs(Integer page, Integer rows, SupplierDrugs supplierDrugs){
         return purchaseServiceWeb.querySupplierDrugs(page,rows,supplierDrugs);
     }
 
@@ -65,7 +69,25 @@ public class PurchaseController {
     public List<SupplierDrugs> querySuppliers(){
         return purchaseServiceWeb.querySuppliers();
     }
+    /**
+     * 查询采购订单供应商
+     * @return
+     */
+    @RequestMapping("queryOrderManufacturer")
+    @ResponseBody
+    public List<PurchaseOrder> queryOrderManufacturer(){
+        return purchaseServiceWeb.queryOrderManufacturer();
+    }
 
+    /**
+     * 查询出所有金额
+     * @return
+     */
+    @RequestMapping("queryConsumption")
+    @ResponseBody
+    public List<ShoppingCart> queryConsumption(){
+        return purchaseServiceWeb.queryConsumption();
+    }
     /**
      * 查询购物车列表
      * @param page
@@ -73,12 +95,23 @@ public class PurchaseController {
      * @param shoppingCart
      * @return
      */
-    @GetMapping("queryShoppingCart")
+    @RequestMapping("queryShoppingCart")
     @ResponseBody
-    public HashMap<String, Object> queryShoppingCart(@RequestParam("page") Integer page, @RequestParam("rows") Integer rows,@RequestBody ShoppingCart shoppingCart){
+    public HashMap<String, Object> queryShoppingCart(Integer page, Integer rows, ShoppingCart shoppingCart){
         return purchaseServiceWeb.queryShoppingCart(page,rows,shoppingCart);
     }
 
+    /**
+     * 删除购物车的信息
+     * @param id
+     * @return
+     */
+    @RequestMapping("deleteOrder")
+    @ResponseBody
+    public Boolean deleteOrder(Integer id){
+        purchaseServiceWeb.deleteOrder(id);
+        return true;
+    }
     /**
      * 采购订单查询
      * @param purchaseOrder
@@ -86,21 +119,48 @@ public class PurchaseController {
      */
     @RequestMapping("queryPurchaseOrder")
     @ResponseBody
-    public List<PurchaseOrder> queryPurchaseOrder(@RequestBody PurchaseOrder purchaseOrder){
+    public List<PurchaseOrder> queryPurchaseOrder(PurchaseOrder purchaseOrder){
         return purchaseServiceWeb.queryPurchaseOrder(purchaseOrder);
     }
 
+    /**
+     * 加入购物车
+     * 加入采购订单
+     * @param id
+     * @param boxCount
+     * @return
+     */
     @RequestMapping("addToCart")
     @ResponseBody
     public Boolean addToCart(Integer id,Integer boxCount){
-        SupplierDrugs supplierDrugs = purchaseServiceWeb.queryToCart(id);
+        SupplierDrugs supplierDrugs = purchaseServiceWeb.queryToCart(id);//根据id回显对应的商品信息
         ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.setCount(boxCount);
-        shoppingCart.setName(supplierDrugs.getDrugName());
-        shoppingCart.setUnitPrice(supplierDrugs.getPurchasePrice());
-        shoppingCart.setSubtotal(boxCount*supplierDrugs.getPurchasePrice());
-        shoppingCart.setSpecifications(supplierDrugs.getSpecifications());
-        purchaseServiceWeb.addToCart(shoppingCart);
+        shoppingCart.setCount(boxCount);//数量
+        shoppingCart.setName(supplierDrugs.getDrugName());//商品名称
+        shoppingCart.setUnitPrice(supplierDrugs.getPurchasePrice());//单价
+        shoppingCart.setSubtotal(boxCount*supplierDrugs.getPurchasePrice());//小计
+        shoppingCart.setSpecifications(supplierDrugs.getSpecifications());//规格
+        purchaseServiceWeb.addToCart(shoppingCart);         //加入购物车
+        purchaseServiceWeb.updateSupplierDrugs(id,boxCount);//修改商品库存
+
+        //uuid
+        int machineId = 1;
+        int hashCodeV = UUID.randomUUID().toString().hashCode();
+        if(hashCodeV < 0) {
+            hashCodeV = - hashCodeV;
+        }
+        String str = new SimpleDateFormat("yyyyMMdd").format(new Date())+machineId + String.format("%012d", hashCodeV);
+
+        PurchaseOrder purchaseOrder = new PurchaseOrder();
+        purchaseOrder.setOrderTime(new Date());//时间
+        purchaseOrder.setOrderNumber(str);//订单号
+        purchaseOrder.setManufacturer(supplierDrugs.getManufacturer());//生产厂家
+        purchaseOrder.setName(supplierDrugs.getDrugName());//商品名称
+        purchaseOrder.setUnitPrice(supplierDrugs.getPurchasePrice());//单价
+        purchaseOrder.setCount(boxCount);//数量
+        purchaseOrder.setActualPayment(boxCount*supplierDrugs.getPurchasePrice());//实付款
+        purchaseOrder.setStatus(1);//状态
+        purchaseServiceWeb.addPurchaseOrder(purchaseOrder);//加入采购订单
         return true;
     }
 
