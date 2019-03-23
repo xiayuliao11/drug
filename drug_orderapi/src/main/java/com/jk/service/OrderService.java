64ecad863917ea173878a104f9763c2655df73ac
1969321229 <1969321229@qq.com>
2019-03-23 15:15:24 +0800

    1234

--- drug_orderapi/src/main/java/com/jk/service/OrderService.java
+++ drug_orderapi/src/main/java/com/jk/service/OrderService.java
@@ -1,10 +1,7 @@
 package com.jk.service;
 
 import com.jk.pojo.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -30,4 +27,25 @@ public interface OrderService {
 
     @PostMapping("addDemand")
     void addDemand(Demand t);
+
+    @PostMapping("loninGood")
+    void addShoppingCart(ShoppingCart shoppingCart);
+
+    @GetMapping("logupdateSl")
+    void logupdateSl(@RequestParam("state") Integer state,@RequestParam("id") Integer id,@RequestParam("a") Integer a);
+
+    @GetMapping("logjieSuanCheckPrice")
+    List<ShoppingCart> logjieSuanCheckPrice(@RequestParam("ids") String ids,@RequestParam ("a") Integer a);
+
+    @GetMapping("loggouwuche")
+    List<ShoppingCart> loggouwuche(@RequestParam("yhid")Integer yhid);
+
+    @DeleteMapping("logdeleteItem")
+    void logdeleteItem(@RequestParam("id") Integer id,@RequestParam("a") Integer a);
+
+    @GetMapping("getShoppingCartKC")
+    ShoppingCart getShoppingCartKC(@RequestParam("id") Integer id);
+
+    @GetMapping("getQuery")
+    UserBean getQuery(@RequestParam("account") String account,@RequestParam("userPwd") String userPwd);
 }
